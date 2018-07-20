19940892d63e4d2389097569d33537fc8dddbf8b
Sharad Rawat <rawat.33@osu.edu>
2018-07-19 23:23:55 -0400

    Update FusionEKF.cpp

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -134,7 +134,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
-  float dt =  measurement_pack.timestamp_ - previous_timestamp_/ 1000000.0;
+  float dt =  (measurement_pack.timestamp_ - previous_timestamp_)/ 1000000.0;
   previous_timestamp_ = measurement_pack.timestamp_;
   float noise_ax = 9;
   float noise_ay = 9;
